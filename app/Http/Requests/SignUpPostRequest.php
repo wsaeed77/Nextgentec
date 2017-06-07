5ce1a75070e89f3205af965362f13812ebdd5012
Laravel Shift <shift@laravelshift.com>
2017-06-07 12:06:28 +0000

    Shift Form Requests

--- app/Http/Requests/SignUpPostRequest.php
+++ app/Http/Requests/SignUpPostRequest.php
@@ -2,9 +2,10 @@
 
 namespace App\Http\Requests;
 
+use Illuminate\Foundation\Http\FormRequest;
 use App\Http\Requests\Request;
 
-class SignUpPostRequest extends Request
+class SignUpPostRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
--- app/Modules/Employee/Http/Requests/EmployeePostRequest.php
+++ app/Modules/Employee/Http/Requests/EmployeePostRequest.php
@@ -2,9 +2,10 @@
 
 namespace App\Modules\Employee\Http\Requests;
 
+use Illuminate\Foundation\Http\FormRequest;
 use App\Http\Requests\Request;
 
-class EmployeePostRequest extends Request
+class EmployeePostRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
